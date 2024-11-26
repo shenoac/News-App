f87d9fbf5e28c4cbcd4f093dc481e7095790c2a7
FrancieliSauthier <francieli.sauthier@gmail.com>
2024-11-26 12:45:59 +0100

    add test to user profile and update auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,27 +1,36 @@
-import { Request, Response, NextFunction } from "express"
+import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import {configs} from '../config/env.js' 
-const authMiddleWare = (req: Request, res: Response, next:NextFunction) => {
-    const token = req.headers.authorization?.split(" ")[1];
-    if(!token) {
-        res.status(401).send({message: "No auth Token provided"});
-        return;
-    }
-    try{
-        if(!configs.auth.JWT_SECRET) {
-            throw new Error('Error in verifing the token');
-        }
-        jwt.verify(token, configs.auth.JWT_SECRET, (error, decode) => {
-            if(error) {
-                res.status(401).send({message: "Invalid token"});
-                return;
-            }
-            req.user = decode as {id: number};
-        });
-        next();
-    } catch(error) {
-        res.status(500).send({message: "A Server Error occured", error})
-    }
-}
 
-export default authMiddleWare;
+import { configs } from '../config/env.js';
+
+export const authMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) {
+    res.status(401).send({ message: 'No auth token provided' });
+    return;
+  }
+  try {
+    if (!configs.auth.JWT_SECRET) {
+      throw new Error('Error in verifing the token');
+    }
+    const decode = jwt.verify(token, configs.auth.JWT_SECRET);
+    req.user = decode as { userId: number };
+    next();
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.name === 'TokenExpiredError') {
+        res.status(401).send({ message: 'Token has expired' });
+        return;
+      }
+      if (error.name === 'JsonWebTokenError') {
+        res.status(401).send({ message: 'Invalid token' });
+        return;
+      }
+      res.status(500).send({ message: 'A server error occured', error });
+    }
+  }
+};
--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,25 +1,93 @@
-import app from "../index.js"
-import request from "supertest"
-import { AppDataSource } from "../config/database.js"
-import {faker} from '@faker-js/faker';
+import request from 'supertest';
+import { faker } from '@faker-js/faker';
+import jwt from 'jsonwebtoken';
 
-beforeAll(async ()=> {
-    await AppDataSource.initialize();
-})
+import app from '../index.js';
+import { AppDataSource } from '../config/database.js';
+import { User } from '../entities/User.js';
+import { configs } from '../config/env.js';
 
-afterAll(async ()=> {
-    await AppDataSource.destroy();
-})
+let randomUser: User;
+let validToken: string;
+let expiredToken: string;
+let invalidToken: string;
+const getUserProfileURL = '/api/users/profile';
 
-describe("User Tests", () => {
-    const randomUser = {
-        email: faker.internet.email(),
-        password: faker.internet.password(),
-        name: faker.person.fullName()
-    }
-    it("should register a User", async () => {
-        const res = await request(app).post('/api/users/register').send(randomUser);
-        expect(res.status).toBe(201);
-        expect(res.body.message).toBe("User Registered Succefully");
+beforeAll(async () => {
+  await AppDataSource.initialize();
+
+  const newUser = {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    name: faker.person.fullName(),
+  };
+  const savedUser = await AppDataSource.getRepository(User).save(newUser);
+  randomUser = savedUser;
+
+  validToken = jwt.sign({ userId: randomUser.id }, configs.auth.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+  expiredToken = jwt.sign({ userId: randomUser.id }, configs.auth.JWT_SECRET, {
+    expiresIn: '-1h',
+  });
+  invalidToken = jwt.sign({ userId: 9999 }, configs.auth.JWT_SECRET, {
+    expiresIn: '1h',
+  });
+});
+
+afterAll(async () => {
+  await AppDataSource.getRepository(User).delete({});
+  await AppDataSource.destroy();
+});
+
+describe('User test', () => {
+  const randomUser = {
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    name: faker.person.fullName(),
+  };
+
+  it('should register a user', async () => {
+    const res = await request(app).post('/api/users/register').send(randomUser);
+    expect(res.status).toBe(201);
+    expect(res.body.message).toBe('User registered');
+  });
+});
+
+describe('User profile test', () => {
+  it('should retrive a user profile when a valid token is sent', async () => {
+    const res = await request(app)
+      .get(getUserProfileURL)
+      .set('Authorization', `Bearer ${validToken}`);
+    expect(res.status).toBe(200);
+    expect(res.body.user).toMatchObject({
+      id: randomUser.id,
+      email: randomUser.email,
+      name: randomUser.name,
     });
-})
+  });
+
+  it('should not retrive a user profile when a token is not sent', async () => {
+    const res = await request(app)
+      .get(getUserProfileURL)
+      .set('Authorization', `Bearer `);
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe('No auth token provided');
+  });
+
+  it('should not retrive a user profile when a user is not found', async () => {
+    const res = await request(app)
+      .get(getUserProfileURL)
+      .set('Authorization', `Bearer ${invalidToken}`);
+    expect(res.status).toBe(404);
+    expect(res.body.message).toBe('User not founf');
+  });
+
+  it('should not retrive a user profile when the token is expired', async () => {
+    const res = await request(app)
+      .get(getUserProfileURL)
+      .set('Authorization', `Bearer ${expiredToken}`);
+    expect(res.status).toBe(401);
+    expect(res.body.message).toBe('Token has expired');
+  });
+});
