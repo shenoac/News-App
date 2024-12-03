45b34bad1e8c928a037dbb1eeaaa147cfb0ed763
shenoac <shenoachee@gmail.com>
2024-12-03 11:19:04 +0100

    Resolve conflict in src/utils.ts, keep upstream changes

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+
 import { configs } from './config/env.js';
 export const fetchFromNewsAPI = async (endpoint: string, params: any) => {
   const url = `https://newsapi.org/v2${endpoint}`;
@@ -8,5 +9,6 @@ export const fetchFromNewsAPI = async (endpoint: string, params: any) => {
       apikey: configs.NEWS_API_KEY,
     },
   });
+
   return response.data;
 };
