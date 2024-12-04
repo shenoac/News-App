6dfe3670dab2ccaa9c3dd8777494458cd1ffaa5d
bojana-petroska <bojana.petroska@yahoo.com>
2024-12-04 10:50:20 +0100

    controller for personalized news added

--- src/modules/News/controller.ts
+++ src/modules/News/controller.ts
@@ -13,4 +13,36 @@ const getLatestNews = async (req: Request, res: Response) => {
   }
 };
 
-export default { getLatestNews };
+const getPersonalizedNews = async (req: Request, res: Response) => {
+  const { categories, limit = 10 } = req.query;
+
+  if (!categories) {
+    res
+      .status(400)
+      .send({ message: 'Missing required query parameter: categories' });
+    return;
+  }
+
+  try {
+    const news = await fetchFromNewsAPI('/everything', {
+      q: categories,
+      limit,
+    });
+
+    const formattedResponse = news.articles.map((article: any) => ({
+      id: article.id,
+      title: article.title,
+      description: article.description,
+      url: article.url,
+      source: article.source.name,
+      publishedAt: article.publishedAt,
+    }));
+    res.status(200).send(formattedResponse);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: 'Error in getting personalized news', error });
+  }
+};
+
+export default { getLatestNews, getPersonalizedNews };
--- src/modules/News/routes.ts
+++ src/modules/News/routes.ts
@@ -3,11 +3,13 @@ import newsValidationSchemas from './validation.js';
 import { validateRequest } from '../../middleware/validateRequest.js';
 import newsController from './controller.js';
 const router = Router();
+import auth from '../../middleware/auth.js';
 
 router.get(
   '/latest',
   validateRequest(newsValidationSchemas.latestNews),
   newsController.getLatestNews,
 );
+router.get('/personalized', auth, newsController.getPersonalizedNews);
 
 export default router;
